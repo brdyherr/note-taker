51c403494e75aa6fa9457a3485dbb2da34978a79
Brown Mamba <brdyherrera@gmail.com>
2021-04-06 02:07:56 -0400

    added get route and added data to db

---
+++ routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const { createNewNote } = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
+
+
+router.get('/notes', (req, res) => {
+    const results = notes;
+    res.json(results);
+});
+
+module.exports = router;
