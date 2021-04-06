3af19ceffc0bb5c9274f2cfad82d2a69c505c75a
Brown Mamba <brdyherrera@gmail.com>
2021-04-06 02:45:26 -0400

    set the html post validation

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,7 +3,17 @@ const { createNewNote, validateNote } = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
 
 
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
 
+    if (!validateNote(req.body)) {
+        res.status(400).send('The Note is not properly formatted.');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
 
 
 module.exports = router;
