ecf21429a9c83c56d50211275c23a2caddcc9514
Brown Mamba <brdyherrera@gmail.com>
2021-04-06 03:24:09 -0400

    resolved 404 error and now able to display notes.html

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
+const path = require('path');
 const { createNewNote, validateNote } = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
 
+
+router.get('/notes', (req, res) =>{
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
 
 router.post('/notes', (req, res) => {
     // set id based on what the next index of the array will be
