5ff6603e685b00c2ab6e22a5b336f3779e785dc6
Brown Mamba <brdyherrera@gmail.com>
2021-04-06 02:23:11 -0400

    added validation to notes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
-const { createNewNote } = require('../../lib/notes');
+const { createNewNote, validateNote } = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
 
 
 router.get('/notes', (req, res) => {
     const results = notes;
     res.json(results);
+});
+
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted!');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
 });
 
 module.exports = router;
